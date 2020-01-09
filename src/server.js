75923b600cb3f2f3d2d24f0d9b2ca0dea1d0aa22
MonnJeongMin <56195084+MonnJeongMin@users.noreply.github.com>
2020-01-09 16:44:48 +0900

    statkc setting 및 socket.io 설정

--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
 import {join} from "path";
 import express from "express";
+import socketIO from "socket.io";
 
+//Setting
 const PORT = 4000;
 const app = express();
-
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
 
+//middleware
+//static 관련
+app.use(express.static(join(__dirname,"static")));
+
+//routing
 app.get("/", (req, res)=> res.render("home"));
 
+//Server start action
 const handleListening = () => console.log(`✔ Server is runnign! http://localhost:${PORT}`);
-
 app.listen(PORT, handleListening);
