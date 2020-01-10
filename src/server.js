7fc649faba6c459d6dde22604c6c92d5521d14af
MonnJeongMin <56195084+MonnJeongMin@users.noreply.github.com>
2020-01-10 14:47:33 +0900

    chatting event handler create 1. sendMessage 메시지를 보낼때 쓰는 함수 2. handleMessageNortifi 메시지를 받아서 다른이들에게 뿌려줌

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 //Setting
 const PORT = 4000;
@@ -10,17 +11,25 @@ app.set("views", join(__dirname, "views"));
 
 //middleware
 //static 관련
-app.use(express.static(join(__dirname,"static")));
+app.use(logger("dev"));
+app.use(express.static(join(__dirname, "static")));
 
 //routing
-app.get("/", (req, res)=> res.render("home"));
+app.get("/", (req, res) => res.render("home"));
 
 //Server start action
-const handleListening = () => console.log(`✔ Server is running! http://localhost:${PORT}`);
+const handleListening = () =>
+  console.log(`✔ Server is running! http://localhost:${PORT}`);
 const server = app.listen(PORT, handleListening);
 
 //io가 모든 이벤트를 보내는 것 받는 것을 하기 위한 변수
 const io = socketIO.listen(server);
 
-//Event emmiter 이벤트를 만드는 것.
-io.on("connection", () => console.log("✔ Somebody Connected!"));
+//Event emmiter 이벤트를 만드는 것. 연결을 하기 위한 이벤트를 만들어진 것
+// on이 만드는 것, emit은 사용하는 것
+io.on("connection", socket => {
+  // chat event handler //
+  socket.on("newMessage", message => {
+    socket.broadcast.emit("messageNotifi", { message })
+  });
+});
--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,15 @@
-영환이 바보
+// eslint-disable-next-line no-undef
+const socket = io("/");
+
+// new message event func
+function sendMessage(message) {
+  socket.emit("newMessage", message);
+}
+
+// recevied new message event func
+function handleMessageNotifi(data) {
+  const { message } = data;
+  console.log(`Client : ${message}`);
+}
+
+socket.on("messageNotifi", handleMessageNotifi);
