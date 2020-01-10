848ce0e8833f7ff4a2f6a981fedd3679913b9da4
MonnJeongMin <56195084+MonnJeongMin@users.noreply.github.com>
2020-01-10 11:47:58 +0900

    npm i morgan 로깅을 해주는 미들웨어로 터미널에 로그를 남겨준다.

--- src/server.js
+++ src/server.js
@@ -16,5 +16,11 @@ app.use(express.static(join(__dirname,"static")));
 app.get("/", (req, res)=> res.render("home"));
 
 //Server start action
-const handleListening = () => console.log(`✔ Server is runnign! http://localhost:${PORT}`);
-app.listen(PORT, handleListening);
+const handleListening = () => console.log(`✔ Server is running! http://localhost:${PORT}`);
+const server = app.listen(PORT, handleListening);
+
+//io가 모든 이벤트를 보내는 것 받는 것을 하기 위한 변수
+const io = socketIO.listen(server);
+
+//Event emmiter 이벤트를 만드는 것.
+io.on("connection", () => console.log("✔ Somebody Connected!"));
